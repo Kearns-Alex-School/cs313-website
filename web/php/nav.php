3b0f0d8192e84bfa9c87ac96b38ffff71c006158
Alex Kearns <alex.j.kearns@gmail.com>
2019-09-25 23:00:07 -0400

    first attempt to use a php reference

---
+++ web/php/nav.php
@@ -0,0 +1,6 @@
+<!-- Navigation Bar -->
+
+<div class="btn-group">
+    <button type="button" class="btn btn-secondary" onclick="">Home</button>
+    <button type="button" class="btn btn-secondary" id="";>Assignments</button>
+<div>
