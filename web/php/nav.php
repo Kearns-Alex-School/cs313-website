d9e8c1c7c5690641713ebbf62fac541da3d410ca
Alex Kearns <alex.j.kearns@gmail.com>
2019-09-26 19:54:59 -0400

    next attempt with using PHP as common file

--- web/php/nav.php
+++ web/php/nav.php
@@ -1,6 +1,10 @@
 <!-- Navigation Bar -->
 
-<div class="btn-group">
-    <button type="button" class="btn btn-secondary" onclick="">Home</button>
-    <button type="button" class="btn btn-secondary" id="";>Assignments</button>
-<div>
+<div id="nav" class="container">
+
+    <div class="btn-group">
+        <button type="button" class="btn btn-secondary" onclick="">Home</button>
+        <button type="button" class="btn btn-secondary" id="">Assignments</button>
+    </div>
+
+</div>
