f4865d8865fb776542669f552ced96a0d8c96075
Alex Kearns <alex.j.kearns@gmail.com>
2019-09-26 21:26:06 -0400

    fixing buttons to act as links to pages

--- web/php/nav.php
+++ web/php/nav.php
@@ -1,8 +1,8 @@
 <!-- Navigation Bar -->
 
 <div class="btn-group">
-    <button type="button" class="btn btn-secondary" href="../index.html">Home</button>
-    <button type="button" class="btn btn-secondary" href="../html/assignments.html">Assignments</button>
+    <a type="button" class="btn btn-secondary" href="../index.html">Home</a>
+    <a type="button" class="btn btn-secondary" href="../html/assignments.html">Assignments</a>
     <button type="button" class="btn btn-secondary dropdown-toggle dropdown-toggle-split" data-toggle="dropdown">
         <span class="caret"></span>
     </button>
