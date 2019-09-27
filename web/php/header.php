674e694daf63fd72e28937afcb075b697490633e
Alex Kearns <alex.j.kearns@gmail.com>
2019-09-26 21:49:36 -0400

    working on the common header file.

--- web/php/header.php
+++ web/php/header.php
@@ -1,5 +1,5 @@
 <!-- Header -->
 
-<img src="../content/images/profile.jpg" class="float-left" height="600">
-<img src="../content/images/alttp-banner.jpg" class="float-left" height="600">
+<img src="../content/images/profile.jpg" class="float-left" height="300">
+<img src="../content/images/alttp-banner.jpg" class="float-left" height="300">
 <!--img src="../content/images/" class="float-left" height="600"-->
