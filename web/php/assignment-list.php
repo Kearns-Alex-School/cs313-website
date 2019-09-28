dbf2beb6899aabdd57e5881e2fbd2bea11ce489a
Alex Kearns <alex.j.kearns@gmail.com>
2019-09-28 19:44:07 -0400

    fixing picture rotation

--- web/php/assignment-list.php
+++ web/php/assignment-list.php
@@ -1,12 +1,12 @@
 <!-- Assignment List -->
 
 <div id="assignment-list" class="container">
-    <h2 class="text-info">
+    <h2 class="">
         <nav class="nav flex-column">
-            <a class="nav-link disabled" href="#">Assignment</a>
-            <a class="nav-link disabled" href="#">Assignment</a>
-            <a class="nav-link disabled" href="#">Assignment</a>
-            <a class="nav-link disabled" href="#">Assignment</a>
+            <a class="nav-link" href="#">Assignment</a>
+            <a class="nav-link" href="#">Assignment</a>
+            <a class="nav-link" href="#">Assignment</a>
+            <a class="nav-link" href="#">Assignment</a>
         </nav>
     </h2>
 </div>
