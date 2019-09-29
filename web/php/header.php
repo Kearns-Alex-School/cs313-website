c4370f9ef86590852183eb7d5e03d6cf5a27d1d1
Alex Kearns <alex.j.kearns@gmail.com>
2019-09-28 21:06:35 -0400

    fixing pictures

--- web/php/header.php
+++ web/php/header.php
@@ -2,15 +2,15 @@
 <div id="header" class="container">
     <div class="row text-center">
 
-        <div class="col-sm">
+        <div class="col">
             <img src="../content/images/profile.jpg" class="rounded" height="200">
         </div>
 
-        <div class="col-sm">
+        <div class="col">
             <img src="../content/images/alttp-banner.jpg" class="rounded" height="200">
         </div>
 
-        <div class="col-sm">
+        <div class="col">
             <img src="../content/images/slct.jpg" class="rounded r-90" width="200">
         </div>
 
