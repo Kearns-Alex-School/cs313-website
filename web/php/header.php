6bd7fbfa8da6d5d3f5f1e7efbf3a5089acab16b6
Alex Kearns <alex.j.kearns@gmail.com>
2019-09-28 21:14:35 -0400

    fixing pictures

--- web/php/header.php
+++ web/php/header.php
@@ -2,15 +2,15 @@
 <div id="header" class="container">
     <div class="row">
 
-        <div class="col-sm-4">
+        <div class="col-sm-4 bg-success">
             <img src="../content/images/profile.jpg" class="rounded" height="200">
         </div>
 
-        <div class="col-sm-4">
+        <div class="col-sm-4 bg-warning">
             <img src="../content/images/alttp-banner.jpg" class="rounded" height="200">
         </div>
 
-        <div class="col-sm-4">
+        <div class="col-sm-4 bg-danger">
             <img src="../content/images/slct.jpg" class="rounded r-90" width="200">
         </div>
 
