d44ec6be966fcd282396cf84e4ecff5b62593510
Alex Kearns <alex.j.kearns@gmail.com>
2019-10-05 13:03:57 -0400

    debugging js

--- web/js/review-cart.js
+++ web/js/review-cart.js
@@ -12,8 +12,11 @@ function init() {
     var arrayLength = products.length;
 
     for (var i = 0; i < arrayLength; i++) {
-        var subtotal = products[i].price * products[i].qty;
-        text += '<tr><td width="20%">' + products[i].name + '</td><td width="10%">' + products[i].price.toFixed(2) + '</td><td width="20%">' + products[i].qty + '</td><td width="30%">' + subtotal.toFixed(2) + '</td></tr>';
+        var price = products[i].price;
+        var qty = products[i].qty;
+
+        var subtotal = price * qty;
+        text += '<tr><td width="20%">' + products[i].name + '</td><td width="10%">' + price.toFixed(2) + '</td><td width="20%">' + qty + '</td><td width="30%">' + subtotal.toFixed(2) + '</td></tr>';
 
         total += subtotal;
     }
