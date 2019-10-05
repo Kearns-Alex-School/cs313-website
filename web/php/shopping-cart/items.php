abc3f0b2396a32fbaa96636460aec37bd880650e
Alex Kearns <alex.j.kearns@gmail.com>
2019-10-04 22:57:45 -0400

    updating style

--- web/php/shopping-cart/items.php
+++ web/php/shopping-cart/items.php
@@ -83,7 +83,7 @@ $width = 100;
                     <img class="product" src="'.$root.$item[$Picture_Index].'" alt="'.$item[$Name_Index].'" width="'.$width.'">
                 </td>
                 <td width="20%">'.$item[$Name_Index].'</td>
-                <td width="10%" id="item-'.$temp_index.'-price"><p>'.$item[$Price_Index].'</p></td>
+                <td width="10%" id="item-'.$temp_index.'-price">'.$item[$Price_Index].'</td>
                 <td width="20%">
                     <input id="item-'.$temp_index.'-qty" class="form-control" type="number" value="0"/>
                 </td>
