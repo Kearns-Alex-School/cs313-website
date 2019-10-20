8f26823e0bba8c076b2328fd9a48684c4be1f4b4
Alex Kearns <alex.j.kearns@gmail.com>
2019-10-20 01:22:02 -0400

    finishing touches on framework UI

--- web/pages/assignments/db-project/rooms.php
+++ web/pages/assignments/db-project/rooms.php
@@ -66,22 +66,14 @@ $db = get_db();
 
             // Search 
             $roomName = $_GET['roomName'];
-            $stmt = $db->prepare('select * from t_room');
 
-            if ($visited == false || $roomName === '')
+            if ($visited == false)
             {
                 $stmt = $db->prepare('select * from t_room');
             }
             else {
                 $stmt = $db->prepare("select * from t_room WHERE room_name like '%:roomName%");
                 $stmt->bindValue(':roomName', $roomName, PDO::PARAM_STR);
-            }
-
-            if ($roomName !== '' || $visited == false)
-            {
-                
-            } else {
-                $stmt = $db->prepare('select * from t_room');
             }
             
             $stmt->execute();
