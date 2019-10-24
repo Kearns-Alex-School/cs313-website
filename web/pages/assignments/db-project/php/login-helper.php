f946095ea739fb048e27c0c41c77ca58494ccad8
Alex Kearns <alex.j.kearns@gmail.com>
2019-10-24 01:14:31 -0400

    Debugging php

--- web/pages/assignments/db-project/php/login-helper.php
+++ web/pages/assignments/db-project/php/login-helper.php
@@ -19,11 +19,15 @@ echo '<p>pass: ' . $password . '</p>';
 // if we are creating a new user then we need to add it so we will pass on the "check" later
 if ($submit === 'create' && $password !== '' && $username !== '') {
 	$sql = "insert into t_user (user_name, user_password) values ('" . $username . "', '" . $password . "')";
+    echo '<p>user: ' . $username . '</p>';
+    echo '<p>pass: ' . $password . '</p>';
+    echo '<p>pass: ' . $sql . '</p>';
 
-	$stmt = $db->prepare($sql);
-	$stmt->execute();
+
+	//$stmt = $db->prepare($sql);
+	//$stmt->execute();
 }
-
+/*
 // grab the username from the database if there is a match
 $sql = "select * from t_user where user_name='" . $username . "'";
 
@@ -56,5 +60,5 @@ else {
     header("Location: ../login.php?fail=true");
 
     die();
-}
+}*/
 ?>
