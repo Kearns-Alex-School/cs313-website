892a79b4f701db142f7616dfca53b3da0d2246a8
Alex Kearns <alex.j.kearns@gmail.com>
2019-10-24 22:36:05 -0400

    fixing issues

--- web/pages/assignments/db-project/php/rooms-helper.php
+++ web/pages/assignments/db-project/php/rooms-helper.php
@@ -1,5 +1,5 @@
 <?php
-require "dbConnect.php";
+require "php/dbConnect.php";
 $db = get_db();
 session_start();
 
@@ -25,16 +25,9 @@ switch ($func)
 
 function Refresh() {
     $sql = 'select room_id, room_name from t_room';
-    try
-    {
-        $stmt = $db->prepare($sql);
-    }
-    catch (PDOException $ex) {
-		// If this were in production, you would not want to echo
-		// the details of the exception.
-		echo "Error connecting to DB. Details: $ex";
-		return;
-	}
+
+    // trouble line
+    $stmt = $db->prepare($sql);
 
     echo 'indoRefresh';
     //GetRows($stmt);
