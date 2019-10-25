4b3c7b1d2aa8be786dd06e27bee8cb9abdd1d45a
Alex Kearns <alex.j.kearns@gmail.com>
2019-10-24 22:46:20 -0400

    fixing issues

--- web/pages/assignments/db-project/php/rooms-helper.php
+++ web/pages/assignments/db-project/php/rooms-helper.php
@@ -3,4 +3,33 @@ require "dbConnect.php";
 $db = get_db();
 session_start();
 
+$func = htmlspecialchars($_GET['function']);
+
+switch ($func)
+{
+    case "search":
+        //Search();
+        break;
+
+    case "refresh":
+        Refresh();
+        break;
+
+    case "create":
+        //Create();
+        break;
+
+    default:
+        echo 'fail';
+}
+
+function Refresh() {
+    //$sql = 'select room_id, room_name from t_room';
+
+    // trouble line
+    //$stmt = $db->prepare($sql);
+
+    echo 'indoRefresh';
+    //GetRows($stmt);
+}
 ?>
