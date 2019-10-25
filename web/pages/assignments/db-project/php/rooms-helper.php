892757f19ca15cd4998f16d8be53a39d71cc64c6
Alex Kearns <alex.j.kearns@gmail.com>
2019-10-24 22:23:49 -0400

    fixing issues

--- web/pages/assignments/db-project/php/rooms-helper.php
+++ web/pages/assignments/db-project/php/rooms-helper.php
@@ -14,7 +14,7 @@ switch ($func)
 
     case "refresh":
         echo 'refresh';
-        //Refresh();
+        Refresh();
         break;
 
     case "create":
@@ -24,5 +24,41 @@ switch ($func)
 
     default:
         echo 'fail';
+}
+
+function Refresh() {
+    $stmt = $db->prepare('select room_id, room_name from t_room');
+
+    GetRows($stmt);
+}
+
+function GetRows($statement) {
+    /*try
+    {
+        $statement->execute();
+    }
+    catch (PDOException $ex) {
+        echo "Error connecting to DB. Details: $ex";
+        return;
+    }
+    
+    $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+    $html_text = "";
+
+    foreach ($rows as $row)
+    {
+        $room_name = $row['room_name'];
+        $room_id = $row['room_id'];
+
+        $html_text .= '<p>';
+        $html_text .= '<a href="chat.php?room=' . $room_name . '&roomid=' . $room_id .'">';
+        $html_text .= '<b>' . $room_name . '</b>';
+        $html_text .= '</a>';
+        $html_text .= '</p>';
+    }
+
+    // send all of the results back to the caller.
+    echo $html_text;*/
 }
 ?>
