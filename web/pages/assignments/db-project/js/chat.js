9b144d0845dc804fd32bef869e60a78fcc4ea861
Alex Kearns <alex.j.kearns@gmail.com>
2019-10-27 01:15:34 -0400

    adding function for getting to chatroom

--- web/pages/assignments/db-project/js/chat.js
+++ web/pages/assignments/db-project/js/chat.js
@@ -1,16 +1,33 @@
+window.intervalid = '';
+window.instance   = false;
+
+
+intervalid = setInterval('Refresh()', 1000);
+
+
 function Refresh()
 {
-    console.log("inside");
-    var roomid = document.getElementById("roomid").value;
-    var xhttp = new XMLHttpRequest();
+    // check to see if we are already in this update
+    if (!instance)
+    {
+        // set our flag so we do not walk on ourself
+        instance = true;
 
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("results").innerHTML = this.responseText;
-        }
-    };
+        console.log("inside");
+        var roomid = document.getElementById("roomid").value;
+        var xhttp = new XMLHttpRequest();
 
-    xhttp.open("POST", "php/chat-helper.php", true);
-    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhttp.send('function=refresh&id=' + roomid);
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("results").innerHTML = this.responseText;
+
+                // reset the flag
+                instance = false;
+            }
+        };
+
+        xhttp.open("POST", "php/chat-helper.php", true);
+        xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xhttp.send('function=refresh&id=' + roomid);
+    }
 }
